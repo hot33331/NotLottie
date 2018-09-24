5133fbf58b46f51776b57e4ac1f2c853fcba5a31
Tobias Hoppenthaler <tobias.hoppenthaler@gmail.com>
2018-09-24 20:38:29 +0200

    Initial commit

---
+++ NotLottie/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotLottie.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
